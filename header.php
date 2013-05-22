31d8e7e01cea574c58ee27e0fd889cb2288ab49c
Miguel Peixe <miguel@cardume.art.br>
2013-05-22 10:35:16 -0300

    social

--- header.php
+++ header.php
@@ -24,6 +24,14 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(get_bloginfo('language')); ?>>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1&appId=450923021667564";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 	<header id="masthead">
 		<div class="container">
 			<div class="two columns">
