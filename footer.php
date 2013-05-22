5823aeabf3fa00b4027b80a3a324dbfd1dba5269
Miguel Peixe <miguel@cardume.art.br>
2013-05-22 16:35:16 -0300

    fix footer and styles

--- footer.php
+++ footer.php
@@ -16,13 +16,13 @@
 			<div class="cc">
 				<p xmlns:dct="http://purl.org/dc/terms/" xmlns:vcard="http://www.w3.org/2001/vcard-rdf/3.0#">
 					<a rel="license" href="http://creativecommons.org/publicdomain/zero/1.0/">
-						<img src="http://i.creativecommons.org/p/zero/1.0/80x15.png" style="border-style: none;" alt="CC0" />
+						<img src="http://i.creativecommons.org/p/zero/1.0/88x31.png" style="border-style: none;" alt="CC0" class="scale-with-grid" />
 					</a>
 					<span class="text">
 						To the extent possible under law,
 						<a rel="dct:publisher"
 						href="http://arteforadomuseu.com.br/">
-						<span property="dct:title">Andre Deak</span></a>
+						<span property="dct:title">Arte Fora do Museu</span></a>
 						has waived all copyright and related or neighboring rights to
 						<span property="dct:title">Arte Fora do Museu</span>.
 						This work is published from:
@@ -36,10 +36,15 @@
 		<div class="three columns">
 			<div class="social">
 				<div class="social-links">
-					<a href="https://www.facebook.com/pages/Arte-Fora-do-Museu/152356688151751?fref=ts" class="facebook lsf" rel="external" target="_blank">facebook</a>
-					<a href="http://instagram.com/arteforadomuseu" class="instagram lsf" rel="external" target="_blank">instagram</a>
-					<a href="https://twitter.com/arteforadomuseu" class="twitter lsf" rel="external" target="_blank">twitter</a>
-					<a href="http://www.youtube.com/arteforadomuseu" class="youtube lsf" rel="external" target="_blank">youtube</a>
+					<div class="social-container">
+						<div class="social-content">
+							<a href="https://www.facebook.com/pages/Arte-Fora-do-Museu/152356688151751?fref=ts" class="facebook lsf" rel="external" target="_blank">facebook</a>
+							<a href="http://instagram.com/arteforadomuseu" class="instagram lsf" rel="external" target="_blank">instagram</a>
+							<a href="https://twitter.com/arteforadomuseu" class="twitter lsf" rel="external" target="_blank">twitter</a>
+							<a href="http://www.youtube.com/arteforadomuseu" class="youtube lsf" rel="external" target="_blank">youtube</a>
+						</div>
+					</div>
+					<span class="social-toggler lsf">share</span>
 				</div>
 				<div class="social-interaction">
 					<a href="https://twitter.com/arteforadomuseu" class="twitter-follow-button" data-show-count="false" data-lang="pt" data-show-screen-name="false">Seguir @arteforadomuseu</a>
