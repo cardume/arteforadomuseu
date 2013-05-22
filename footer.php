91cf9cacc940f55f800c37018616144aeb1dd6af
Miguel Peixe <miguel@cardume.art.br>
2013-05-22 10:42:39 -0300

    rearrange

--- footer.php
+++ footer.php
@@ -7,16 +7,16 @@
 		</div>
 		<div class="six columns">
 			<div class="social">
-				<div class="social-interaction">
-					<a href="https://twitter.com/arteforadomuseu" class="twitter-follow-button" data-show-count="false" data-lang="pt" data-show-screen-name="false">Seguir @arteforadomuseu</a>
-					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-					<div class="fb-like" data-href="https://www.facebook.com/pages/Arte-Fora-do-Museu/152356688151751" data-send="false" data-layout="button_count" data-width="450" data-show-faces="true" data-font="verdana" data-colorscheme="dark"></div>
-				</div>
 				<div class="social-links">
 					<a href="https://www.facebook.com/pages/Arte-Fora-do-Museu/152356688151751?fref=ts" class="facebook lsf" rel="external" target="_blank">facebook</a>
 					<a href="http://instagram.com/arteforadomuseu" class="instagram lsf" rel="external" target="_blank">instagram</a>
 					<a href="https://twitter.com/arteforadomuseu" class="twitter lsf" rel="external" target="_blank">twitter</a>
 					<a href="http://www.youtube.com/arteforadomuseu" class="youtube lsf" rel="external" target="_blank">youtube</a>
+				</div>
+				<div class="social-interaction">
+					<a href="https://twitter.com/arteforadomuseu" class="twitter-follow-button" data-show-count="false" data-lang="pt" data-show-screen-name="false">Seguir @arteforadomuseu</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+					<div class="fb-like" data-href="https://www.facebook.com/pages/Arte-Fora-do-Museu/152356688151751" data-send="false" data-layout="button_count" data-width="450" data-show-faces="true" data-font="verdana" data-colorscheme="dark"></div>
 				</div>
 			</div>
 		</div>
